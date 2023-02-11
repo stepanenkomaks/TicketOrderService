e0fa2ac2e78e46295339002bfa6f1902b9f1320d
Maxim Stepanenko <maxstepanenko15@gmail.com>
2023-02-11 14:58:53 +0200

    Add swagger annotations for status service

--- status-service/src/main/java/com/stepanenko/statusservice/controller/StatusController.java
+++ status-service/src/main/java/com/stepanenko/statusservice/controller/StatusController.java
@@ -1,6 +1,9 @@
 package com.stepanenko.statusservice.controller;
 
 import com.stepanenko.statusservice.service.StatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/status")
 @RequiredArgsConstructor
+@Tag(name = "Status", description = "The status controller")
 public class StatusController {
 
     private final StatusService statusService;
 
+    @Operation(summary = "Get status for provided order id")
     @GetMapping()
-    public ResponseEntity<String> getStatus(@RequestParam Long orderId) {
+    public ResponseEntity<String> getStatus(@RequestParam @Parameter(example = "1") Long orderId) {
         return ResponseEntity.ok(statusService.getStatus(orderId));
     }
 }
